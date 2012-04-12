27061271c7f5d64e3cbc79d08f7b554c937e0af1
Ryan Brubaker <ryan.brubaker@gmail.com>
2012-04-11 21:45:59 -0500

    Added a thank you page when user submits feedback

--- Code/controllers/contact.py
+++ Code/controllers/contact.py
@@ -2,6 +2,7 @@
 import os
 import webapp2
 
+from google.appengine.api import mail
 from google.appengine.ext.webapp import template
 
 
@@ -14,6 +15,19 @@ class ContactHandler(webapp2.RequestHandler):
       path = os.path.join(os.path.dirname(__file__), '../pages/contact.html')
       self.response.out.write(template.render(path, {}))
    #
+   
+   
+   def post(self):
+      """
+      Send user's input to my email address
+      """
+      mail.send_mail(sender="Code Feedback Form<ryan.brubaker@gmail.com>",
+                    to="Ryan Brubaker <ryan.brubaker@gmail.com>",
+                    subject="Feedback from Speak Code",
+                    body=self.request.get('message'))
+      
+      self.redirect('/contact_thank_you')
+      
 #
 
 
